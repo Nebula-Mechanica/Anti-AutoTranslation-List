d1f96fbfd4646c945de05701f2663c10bd6deb90
Nebula-Mechanica <Nebula-Mechanica@users.noreply.github.com>
2020-02-29 15:38:39 +0700

    Added DuckDuckGo support

--- genlists.sh
+++ genlists.sh
@@ -3,9 +3,17 @@
 # To add domains just write them it in the end of domains.txt file and run this script
 IFS=$'\n'
 
+#Google
 FILTER_TEMPLATE[1]='google.*##.rc:has(a[href*="@@@/"])'
 FILTER_TEMPLATE[2]='google.*##.nrgt:has(a[href*="@@@/"])'
 FILTER_TEMPLATE[3]='google.*##.xpd.mnr-c:has(a[href*="@@@/"])'
+#DuckDuckGo
+FILTER_TEMPLATE[4]='duckduckgo.com##.result[data-domain$="@@@"]'
+FILTER_TEMPLATE[5]='duckduckgo.com##.web-result:has(a[href*="@@@/"])'
+#DuckDuckGo Lite
+FILTER_TEMPLATE[6]='duckduckgo.com##form[action="/lite/"] tbody tr:has(td a[class="result-link"][href*="@@@/"])'
+FILTER_TEMPLATE[7]='duckduckgo.com##form[action="/lite/"] tbody tr:has(td a[class="result-link"][href*="@@@/"]) + tr'
+FILTER_TEMPLATE[8]='duckduckgo.com##form[action="/lite/"] tbody tr:has(td a[class="result-link"][href*="@@@/"]) + tr + tr'
 
 WORKDIR="./workdir"
 HEADER="header.txt"
